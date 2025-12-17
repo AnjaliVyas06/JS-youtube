9e6f65a5ccb811f6c5ce37d0223d9352ded57795
AnjaliVyas06 <vyasanjali2006@gmail.com>
2025-12-17 04:35:30 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14553
+let accountEmail = "anjali@g.com"
+var accountPassword = "2006"
+accountCity = "kashmir"
+let accountState
+
+// accountId = 2
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
