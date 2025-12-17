b77e84534927ba2f534f738f82741602348397a1
AnjaliVyas06 <vyasanjali2006@gmail.com>
2025-12-17 08:42:15 +0000

    with operators

--- 01_basics/03_DataTypeConversion.js
+++ 01_basics/03_DataTypeConversion.js
@@ -1,21 +1,56 @@
-let score =  undefined //null//"33abc" //"33" 
+let score = undefined; //null//"33abc" //"33"
 // console.log(typeof score);
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber)
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1=>true
 //0=>false
 //""=>false
 //"anajli"=>true
 
-let someNumber = 33
-let stringNumbe = String(someNumber) 
-console.log(typeof stringNumbe)
-console.log( stringNumbe)
+let someNumber = 33;
+let stringNumbe = String(someNumber);
+// console.log(typeof stringNumbe)
+// console.log( stringNumbe)
 
+// *** OPERATIONS *** //
+
+let value = -3;
+let negValue = -value;
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = "Anjali!!!";
+
+let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+console.log(gameCounter)
+
+gameCounter++;
+console.log(gameCounter)
+
+++gameCounter;
+console.log(gameCounter)
