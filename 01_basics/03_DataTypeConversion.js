7901b6da6ab144712c507e142dd6beab80ab2839
AnjaliVyas06 <vyasanjali2006@gmail.com>
2025-12-17 08:18:31 +0000

    datatypeConversion

---
+++ 01_basics/03_DataTypeConversion.js
@@ -0,0 +1,21 @@
+let score =  undefined //null//"33abc" //"33" 
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true
+//0=>false
+//""=>false
+//"anajli"=>true
+
+let someNumber = 33
+let stringNumbe = String(someNumber) 
+console.log(typeof stringNumbe)
+console.log( stringNumbe)
+
