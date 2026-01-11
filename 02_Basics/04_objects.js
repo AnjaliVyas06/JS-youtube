2bd7be1ea0d5ca4c171f53e74c1a5550260ca469
AnjaliVyas06 <vyasanjali2006@gmail.com>
2026-01-11 09:16:18 +0000

    json

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,28 @@
+const course ={
+    courseName:"Js in hindi",
+    price:"999",
+    courseInstructor:"Anjali"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course;
+// console.log(courseInstructor);
+// console.log(instructor);
+
+// const navBar = ({company}) =>{  //(props.company)
+
+// }
+// navBar(company = "anjali")
+
+// {
+//    "name":"Anjali",
+//     "courseName":"Js in Hindi",
+//   "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
